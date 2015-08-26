e4069962c1f59e77989782c2c2792e0cc07b05a1
liyatang <li.yatang@163.com>
2015-08-27 00:09:34 +0800

    找到价格截取字段并且将获取的数据保存在文件

--- python/a.py
+++ python/a.py
@@ -10,9 +10,14 @@ URL="http://hq.sinajs.cn/list=AG1512"
 
 NowTime=time.time()
 LastTime=NowTime
+FileName=str(NowTime)
 while True:
     NowTime=time.time()
     if (NowTime-LastTime)>=TIMEVALUE:
         LastTime=NowTime
         GetStr=urlopen(URL).read()
+        f=open(FileName,"a")
+        f.writelines("%f," % NowTime + GetStr)
+        f.close()
         print GetStr+'%f' % NowTime
+        print GetStr[65:69]
