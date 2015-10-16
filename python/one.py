5153a2f479b1821b87caa862cf11c75814caa326
liyatang <liyatang@hymost.com>
2015-10-16 17:14:52 +0800

    休市时间停止运行

--- python/one.py
+++ python/one.py
@@ -23,9 +23,29 @@ dataMat=[]
 More=0
 Empty=0
 
+#m00=time.mktime(time.strptime('2000-01-01 00:00:00',"%Y-%m-%d %H:%M:%S"))
+m23=time.mktime(time.strptime('2000-01-01 02:30:00',"%Y-%m-%d %H:%M:%S"))
+m90=time.mktime(time.strptime('2000-01-01 09:00:00',"%Y-%m-%d %H:%M:%S"))
+m113=time.mktime(time.strptime('2000-01-01 11:30:00',"%Y-%m-%d %H:%M:%S"))
+m133=time.mktime(time.strptime('2000-01-01 13:30:00',"%Y-%m-%d %H:%M:%S"))
+m150=time.mktime(time.strptime('2000-01-01 15:00:00',"%Y-%m-%d %H:%M:%S"))
+m210=time.mktime(time.strptime('2000-01-01 21:00:00',"%Y-%m-%d %H:%M:%S"))
+#m235=time.mktime(time.strptime('2000-01-01 23:59:59',"%Y-%m-%d %H:%M:%S"))
+
 Account={'InitMoney':INITMONEY,'Crash':INITMONEY,'FutureNum':0,'AllMoney':INITMONEY,'OneProfit':0.0,'Profit':0.0,'BuyPrice':0,'SellPrice':0}
 
 while True:
+    cpStrNowTime=time.strftime("%Y-%m-%d %H:%M:%S")
+    cpListNowTime=list(cpStrNowTime)
+    cpListNowTime[:10]=['2','0','0','0','-','0','1','-','0','1']
+    cpStr2000Time=''.join(cpListNowTime)
+    cp2000Time=time.mktime(time.strptime(cpStr2000Time,"%Y-%m-%d %H:%M:%S"))
+    if (cp2000Time>=m23 and cp2000Time<=m90) or (cp2000Time>=m113 and cp2000Time<=m133) or (cp2000Time>=m150 and cp2000Time<=m210):
+        print "休市时间\t".decode('gbk')+cpStrNowTime
+        time.sleep(10)
+        continue
+
+    time.sleep(1)
     NowTime=time.time()
     if (NowTime-LastTime)>=TIMEVALUE:
         LastTime=NowTime
@@ -45,7 +65,7 @@ while True:
 
                 if Account['AllMoney'] < 219.9 :
                     break
-                TimeStyle=time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(int(NowTime)))
+                TimeStyle=time.strftime("%Y-%m-%d %H:%M:%S")
                 if (Account['FutureNum']==-1 and (dataMat[0]-Account['BuyPrice'])*30 >= 180) or (Account['FutureNum']==1 and (dataMat[0]-Account['BuyPrice'])*30 <= -180) :
                     print "********************************************************************************"
                     print '时间\t'.decode('gbk')+TimeStyle
