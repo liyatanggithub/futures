c493e780e2b26fdd66cf76131238dcb8be604a1e
liyatang <liyatang@hymost.com>
2015-10-28 16:06:30 +0800

    增大滤波幅度

---
+++ python/six.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python
+#coding=utf-8
+
+#from urllib import urlopen
+import urllib2
+import socket
+#from numpy import *
+import string
+import time
+import matplotlib.pyplot as plt
+
+TIMEVALUE=6.0
+URL="http://hq.sinajs.cn/list=AG1512"
+
+xMax=100
+yMin=10000
+yMax=0
+socket.setdefaulttimeout(4)
+NowTime=float(int(time.time()))
+LastTime=NowTime
+FirstTime=NowTime
+#FileName=str(NowTime)
+dataMat=[[0],[0]]
+Aaa=0
+Bbb=0
+Ccc=0
+
+#m00=time.mktime(time.strptime('2000-01-01 00:00:00',"%Y-%m-%d %H:%M:%S"))
+m23=time.mktime(time.strptime('2000-01-01 02:30:00',"%Y-%m-%d %H:%M:%S"))
+m90=time.mktime(time.strptime('2000-01-01 09:00:00',"%Y-%m-%d %H:%M:%S"))
+m113=time.mktime(time.strptime('2000-01-01 11:30:00',"%Y-%m-%d %H:%M:%S"))
+m133=time.mktime(time.strptime('2000-01-01 13:30:00',"%Y-%m-%d %H:%M:%S"))
+m150=time.mktime(time.strptime('2000-01-01 15:00:00',"%Y-%m-%d %H:%M:%S"))
+m210=time.mktime(time.strptime('2000-01-01 21:00:00',"%Y-%m-%d %H:%M:%S"))
+#m235=time.mktime(time.strptime('2000-01-01 23:59:59',"%Y-%m-%d %H:%M:%S"))
+
+while True:
+    plt.pause(0.00001)
+    cpStrNowTime=time.strftime("%Y-%m-%d %H:%M:%S")
+    cpListNowTime=list(cpStrNowTime)
+    cpListNowTime[:10]=['2','0','0','0','-','0','1','-','0','1']
+    cpStr2000Time=''.join(cpListNowTime)
+    cp2000Time=time.mktime(time.strptime(cpStr2000Time,"%Y-%m-%d %H:%M:%S"))
+    if (cp2000Time>=m23 and cp2000Time<=m90) or (cp2000Time>=m113 and cp2000Time<=m133) or (cp2000Time>=m150 and cp2000Time<=m210):
+        #print "ÐÝÊÐÊ±¼ä\t".decode('gbk')+cpStrNowTime
+        time.sleep(10)
+        continue
+
+    time.sleep(1)
+    NowTime=time.time()
+    if (NowTime-LastTime)>=TIMEVALUE:
+        LastTime=NowTime
+        try:
+            GetStr=urllib2.urlopen(URL).read()
+        except :
+            print "Get URL ERROR"
+        else:
+            NowPrice = string.atoi(GetStr[65:69])
+            if NowPrice <= Aaa :
+                print "*****************Low*****************"
+            if NowPrice >= Ccc :
+                print "*****************High*****************"
+            if NowPrice > Aaa and NowPrice < Ccc :
+                continue
+            if Ccc < NowPrice :
+                Ccc = NowPrice
+                Bbb = Ccc-3
+                Aaa = Bbb-3
+            if Aaa > NowPrice :
+                Aaa = NowPrice
+                Bbb = Aaa+3
+                Ccc = Bbb+3
+
+            TimeStyle=time.strftime("%Y-%m-%d %H:%M:%S")
+            print TimeStyle+"\t%f"%Bbb
+            dataMat[1].append(Bbb)
+            dataMat[0].append(dataMat[0][-1]+1)
+            if dataMat[0][-1]>=xMax :
+                xMax = xMax +100
+                plt.axis([0, xMax, yMin, yMax])
+            if dataMat[1][-1]<=yMin :
+                yMin = dataMat[1][-1]-10
+                plt.axis([0, xMax, yMin, yMax])
+            if dataMat[1][-1]>=yMax :
+                yMax = dataMat[1][-1]+10
+                plt.axis([0, xMax, yMin, yMax])
+                plt.axis([0, xMax, yMin, yMax])
+            plt.plot(dataMat[0], dataMat[1],color="blue", linewidth=1.0, linestyle="-")
+            plt.pause(0.00001)
